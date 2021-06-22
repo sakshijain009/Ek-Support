f1fd201e8696490cb4cb63665f005f7199251c7c
sakshijain009 <62885857+sakshijain009@users.noreply.github.com>
2021-06-22 14:11:08 +0530

    database

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/eksupport",{useNewUrlParser:true,useUnifiedTopology: true});
+
+/*****************SCHEMAS********************/
+const volunteerSchema={
+	name: String,
+	email: String,
+	contact: Number,
+	city: String,
+	state: String,
+	reason: String,
+	date: Date
+  };
+  
+  const donateSchema={
+	name: String,
+	email: String,
+	contact: Number,
+	city: String,
+	state: String,
+	pincode: String,
+	street: String,
+	quantity: String,
+	delivery: String,
+	date: Date
+  };
+
+/**************Collections****************/
+const volunteer = mongoose.model("volunteer",volunteerSchema);
+const donate = mongoose.model("donate",donateSchema);
 
 //get requests
 app.get("/",(req,res)=>{
@@ -22,6 +52,43 @@ app.get("/donate",(req,res)=>{
 	res.render("donate");
 });
 
+//post request
+app.post("/volunteer",(req,res)=>{
+	const today=new Date();
+	console.log(req.body);
+	const details = new volunteer({
+		name: req.body.name,
+  		email: req.body.email,
+  		contact:req.body.contact,
+  		city:req.body.city,
+		state:req.body.state,
+		reason:req.body.reason,
+  		date:today
+	});
+	console.log(details);
+	details.save();
+	res.redirect("/");
+});
+
+app.post("/donate",(req,res)=>{
+	const today=new Date();
+	console.log(req.body);
+	const details = new donate({
+		name: req.body.name,
+  		email: req.body.email,
+  		contact:req.body.contact,
+  		city:req.body.city,
+		state:req.body.state,
+		pincode: req.body.pincode,
+		address: req.body.street,
+		quantity: req.body.message,
+		delivery: req.body.optradio,
+  		date:today
+	});
+	console.log(details);
+	details.save();
+	res.redirect("/");
+});
 
 //server
 app.listen(3000, function() {
